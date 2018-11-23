30e10b0afe100969eaf61cf55bb0befa06de33b1
Laurence Hurst <hurstla@bb2login01.bb2.cluster>
2018-11-23 19:00:23 +0000

    Test all ciphers using a collection

--- Testing/testCiphers.cpp
+++ Testing/testCiphers.cpp
@@ -2,46 +2,49 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
+#include <memory>
 #include <iostream>
+#include <vector>
 
 #include "Cipher.hpp"
-#include "CaesarCipher.hpp"
-#include "PlayfairCipher.hpp"
-#include "VigenereCipher.hpp"
+#include "CipherFactory.hpp"
+#include "CipherType.hpp"
+#include "CipherMode.hpp"
 
-bool testCipher( const Cipher& cipher, const CipherMode mode, const std::string& inputText, const std::string& outputText ) {
-	std::string cipher_out = cipher.applyCipher(inputText, mode);
-	std::cout << "Cipher output: " << cipher_out << std::endl;
+bool testCipher( const std::unique_ptr<Cipher>& cipher_ptr, const CipherMode mode, const std::string& inputText, const std::string& outputText ) {
+	std::cout << "Input text: " << inputText;
+	std::string cipher_out = cipher_ptr->applyCipher(inputText, mode);
+	std::cout << " - cipher output: " << cipher_out << std::endl;
 	return ( cipher_out == outputText);
 }
 
-TEST_CASE("Playfair Cipher encryption", "[ciphers]") {
-  PlayfairCipher cc{"hello"};
-  REQUIRE( testCipher(cc, CipherMode::Encrypt, "BOBISSOMESORTOFJUNIORCOMPLEXXENOPHONEONEZEROTHING", "FHIQXLTLKLTLSUFNPQPKETFENIOLVSWLTFIAFTLAKOWATEQOKPPA") );
+struct testCase {
+	std::string inputText;
+	std::string outputText;
+};
+
+TEST_CASE("Ciphers test", "[ciphers]") {
+	std::vector<std::unique_ptr<Cipher>> ciphers;
+	// turn into a vector of vectors of test cases if multiple tests per cipher desired
+	std::vector<testCase> encryption_tests;
+	std::vector<testCase> decryption_tests;
+	
+	ciphers.push_back( cipherFactory( CipherType::Playfair, "hello" ) );
+	encryption_tests.push_back( testCase {"BOBISSOMESORTOFJUNIORCOMPLEXXENOPHONEONEZEROTHING", "FHIQXLTLKLTLSUFNPQPKETFENIOLVSWLTFIAFTLAKOWATEQOKPPA"} );
+	decryption_tests.push_back( testCase {"FHIQXLTLKLTLSUFNPQPKETFENIOLVSWLTFIAFTLAKOWATEQOKPPA", "BOBISXSOMESORTOFIUNIORCOMPLEXQXENOPHONEONEZEROTHINGZ"} );
+	
+	ciphers.push_back( cipherFactory( CipherType::Caesar, "10") );
+	encryption_tests.push_back( testCase {"HELLOWORLD", "ROVVYGYBVN"} );
+	decryption_tests.push_back( testCase {"ROVVYGYBVN", "HELLOWORLD"} );
+	
+	ciphers.push_back( cipherFactory( CipherType::Vigenere, "key") );
+	encryption_tests.push_back( testCase {"HELLOWORLD", "RIJVSUYVJN"} );
+	decryption_tests.push_back( testCase {"RIJVSUYVJN", "HELLOWORLD"} );
+	
+	for ( size_t i = 0; i < ciphers.size(); i++ ) {
+		REQUIRE( testCipher( ciphers[i], CipherMode::Encrypt, encryption_tests[i].inputText, encryption_tests[i].outputText ) );
+		REQUIRE( testCipher( ciphers[i], CipherMode::Decrypt, decryption_tests[i].inputText, decryption_tests[i].outputText ) );
+	}
 }
 
-TEST_CASE("Playfair Cipher decryption", "[ciphers]") {
-  PlayfairCipher cc{"hello"};
-  REQUIRE( testCipher(cc, CipherMode::Decrypt, "FHIQXLTLKLTLSUFNPQPKETFENIOLVSWLTFIAFTLAKOWATEQOKPPA", "BOBISXSOMESORTOFIUNIORCOMPLEXQXENOPHONEONEZEROTHINGZ") );
-}
-
-TEST_CASE("Caesar Cipher encryption", "[ciphers]") {
-  CaesarCipher cc{10};
-  REQUIRE( testCipher(cc, CipherMode::Encrypt, "HELLOWORLD", "ROVVYGYBVN") );
-}
-
-TEST_CASE("Caesar Cipher decryption", "[ciphers]") {
-  CaesarCipher cc{10};
-  REQUIRE( testCipher(cc, CipherMode::Decrypt, "ROVVYGYBVN", "HELLOWORLD") );
-}
-
-TEST_CASE("Vigenere Cipher encryption", "[ciphers]") {
-  VigenereCipher cc{"key"};
-  REQUIRE( testCipher(cc, CipherMode::Encrypt, "HELLOWORLD", "RIJVSUYVJN") );
-}
-
-TEST_CASE("Vigenere Cipher decryption", "[ciphers]") {
-  VigenereCipher cc{"key"};
-  REQUIRE( testCipher(cc, CipherMode::Decrypt, "RIJVSUYVJN", "HELLOWORLD") );
-}
 
